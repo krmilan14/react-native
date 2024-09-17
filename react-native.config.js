b14dad3aacc013f0fb35fcd4b7951ffc9018563f
krmilan14 <work.milanteachahead@gmail.com>
2024-09-17 10:13:14 +0530

    Create react-native.config.js

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  project: {
+    android: {
+      packageName: 'com.invoice', // Ensure this is your correct package name
+    },
+  },
+  dependencies: {
+    // Add any custom dependencies that need manual linking
+  },
+};
